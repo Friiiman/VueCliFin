dfc9f1e6840554f75d3614d1ab0ee7e22dfceec6
Friiiman <friiiman94@gmail.com>
2023-11-30 20:51:14 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BlogCards />
+    <TagsFilter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogCards from './components/BlogCards.vue'
+import TagsFilter from './components/TagsFilter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogCards,
+    TagsFilter
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!-- <style lang="scss"></style> -->
