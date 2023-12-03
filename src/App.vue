662e664fa9555da5c740626a86cd2780bb6f1822
Friiiman <friiiman94@gmail.com>
2023-12-04 00:27:36 +0300

    vue header, footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
+    <PageHeader />
     <BlogCards />
     <TagsFilter />
+    <PageFooter />
   </div>
 </template>
 
 <script>
+import PageHeader from './components/PageHeader.vue'
 import BlogCards from './components/BlogCards.vue'
 import TagsFilter from './components/TagsFilter.vue'
+import PageFooter from './components/PageFooter.vue'
 
 export default {
   name: 'App',
   components: {
+    PageHeader,
     BlogCards,
-    TagsFilter
+    TagsFilter,
+    PageFooter
   }
 }
 </script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="page-header">
+
+        <div class="linear linear__left-linear"></div>
+        <div class="linear linear__right-linear"></div>
+
+        <a href="#" class="page-header__logo">
+            <img src="styles/img/main-logo.svg" alt="main logo">
+        </a>
+        <nav class="main-nav">
+            <ul class="main-nav__list">
+                <li class="main-nav__item">
+                    <a href="#" class="main-nav__link">Home</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="#" class="main-nav__link">Project</a>
+                </li>
+                <li class="main-nav__item">
+                    <a href="blog.html" class="main-nav__link">Blog</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+
+  data () {
+    return {}
+  }
+}
+</script>
