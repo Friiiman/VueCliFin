a9c700e25e07fcb346d297a20ffc0ce2bccb58e1
Friiiman <friiiman94@gmail.com>
2024-02-14 19:35:13 +0300

    slider

--- src/components/ProjectDetailsSlider.vue
+++ src/components/ProjectDetailsSlider.vue
@@ -4,19 +4,28 @@
     <div class="linear linear__left-linear"></div>
     <div class="linear linear__right-linear"></div>
 
-    <div class="project-details-slider__item">
-      <img class="project-details-slider__img" src="styles/img/slider-photo.jpg" alt="project photo">
-    </div>
-    <ul class="project-details-slider__pagination">
-      <li class="project-details-slider__pagination-item"></li>
-      <li class="project-details-slider__pagination-item"></li>
-      <li class="project-details-slider__pagination-item"></li>
-    </ul>
+    <tiny-slider class="project-details-slider__item" :mouse-drag="true" :loop="false" :controls="false" speed="500" gutter="50" items="1">
+        <div class="project-details-slider__img">
+          <img class="project-details-slider__img-item" src="styles/img/slider-photo.jpg" alt="project photo">
+        </div>
+        <div class="project-details-slider__img">
+          <img class="project-details-slider__img-item" src="styles/img/slider-photo.jpg" alt="project photo">
+        </div>
+        <div class="project-details-slider__img">
+          <img class="project-details-slider__img-item" src="styles/img/slider-photo.jpg" alt="project photo">
+        </div>
+    </tiny-slider>
+
   </div>
 </template>
 
 <script>
+import VueTinySlider from 'vue-tiny-slider'
+
 export default {
-  name: 'ProjectDetailsSlider'
+  name: 'ProjectDetailsSlider',
+  components: {
+    'tiny-slider': VueTinySlider
+  }
 }
 </script>
